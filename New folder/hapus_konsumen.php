9f0ca429697ccc2dcf541506f0ea296b9525a5fd
yorisb <56858620+yorisb@users.noreply.github.com>
2026-05-11 16:37:19 +0700

    Delete New folder directory

--- New folder/hapus_konsumen.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conn = new mysqli("srv143.niagahoster.com", "n1572337_developer", "bad_cookies8080", "n1572337_developer");
-
-$kode = $_GET['kode'];
-$conn->query("DELETE FROM calon_konsumen WHERE kode = '$kode'");
-
-header("Location: daftar_konsumen.php");
-exit;
